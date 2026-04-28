7aa1f07aff968cffe363bc2f9636c13e52adacd5
Kusuma M G <kusumamg07@gmail.com>
2026-04-28 19:33:09 +0530

    fix render port

--- app.py
+++ app.py
@@ -80,7 +80,8 @@ def delete(id):
     return redirect('/')
 
 # ---------- START ----------
-if __name__ == '__main__':
+import os
+
+if __name__ == "__main__":
     init_db()
-    if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
